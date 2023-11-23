33528064edfa6e1543514cf5bcda1fbbe09abddd
ilia1234559 <63801700+ilia1234559@users.noreply.github.com>
2023-11-23 21:44:01 +0300

    pushhhh

---
+++ README.md
@@ -0,0 +1,2 @@
+MyString class, which allows you to conveniently work with strings. 
+This class implements overloading of constructors and operators for universal use.
--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ class MyString
 {
 public:
 	MyString()//default constructor
-
 	{
 		str = nullptr;
+		length = 0;
 	}
 
-	MyString(const char *str)//initialision constructor (MyString str("123");)
+	MyString(const char *str)//(MyString str("123") or MyString str = "123")
 	{
-		int length = strlen(str);
+		length = strlen(str);
 		this->str = new char[length + 1];
 
 		for (int i = 0; i < length; ++i)
@@ -23,14 +23,14 @@ public:
 		this->str[length] = '\0';
 	}
 
-	MyString(const MyString& other)// copy constructor
+	MyString(const MyString& other)// copy constructor(MyString str2(str1))
 	{
 		if (this->str != nullptr)
 		{
 			delete[] this->str;
 		}
 
-		int length = strlen(other.str);
+		length = other.length;
 
 		this->str = new char[length + 1];
 
@@ -42,14 +42,22 @@ public:
 		this->str[length] = '\0';
 	}
 
+	MyString(MyString&& other) // move constructor
+	{
+		this->length = other.length;
+		this->str = other.str;
+
+		other.str = nullptr;
+	}
+
 	~MyString()// destructor
 	{
 		delete[] str;
 	}
 
-	MyString& operator = (const char * str) // overloaded operator =  (MyString str1 = "123";)
+	MyString& operator = (const char * str) // overloaded operator =  (str1 = "123");)
 	{
-		int length = strlen(str);
+		length = strlen(str);
 
 		if (this->str != nullptr)
 		{
@@ -68,7 +76,7 @@ public:
 
 	MyString& operator =(const MyString& other)// overloaded operator =  (str1 = str2);
 	{
-		int length = strlen(other.str);
+		this->length = strlen(other.str);
 
 		if (this->str != nullptr)
 		{
@@ -85,54 +93,109 @@ public:
 		}
 
 		this->str[length] = '\0';
+
 		return *this;
 	}
 
 	MyString operator + (const MyString& str2) //overloaded operator + 
 	{
 		MyString newStr;
-		int length = strlen(this->str) + strlen(str2.str);
-		newStr.str = new char[length + 1];
+
+		int thisLength = strlen(this->str);
+		int otherLength = strlen(str2.str);
+
+		newStr.length = thisLength + otherLength;
+
+		newStr.str = new char[thisLength + otherLength + 1];
+		
 		int i = 0;
-		for (; i < strlen(this->str); i++)
+		for (; i < thisLength; ++i)
 		{
 			newStr.str[i] = this->str[i];
 		}
 
-		for (int j = 0; j < strlen(str2.str); ++j, ++i)
+		for (int j = 0; j < otherLength; ++j, ++i)
 		{
 			newStr.str[i] = str2.str[j];
 		}
 
-		newStr.str[length] = '\0';
+		newStr.str[newStr.length] = '\0';
 		return newStr;
+	}
+
+	bool operator == (const MyString& other) //overloaded operator ==
+	{
+		if (this->length != other.length)
+		{
+			return false;
+		}
+
+		for (int i = 0; i < this->length; ++i)
+		{
+			if (this->str[i] != other.str[i])
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	bool operator != (const MyString& other)//overloaded operator !=
+	{
+		return !this->operator ==(other);
+	}
+
+	char& operator [](int index) //overloaded operator []
+	{
+		return this->str[index];
 	}
 
 private://all arrays of char are stored in this variable
 	char* str;
+	int length;
 
 public:
 	void print()
 	{
 		cout << str << endl;
 	}
+
+	int Length()
+	{
+		return length;
+	}
+
+	void replace(char who, char what)
+	{
+		bool isReplace = false;
+		for (int i = 0; i < this->length; ++i)
+		{
+			if (this->str[i] == who)
+			{
+				this->str[i] = what;
+				isReplace = true;
+				cout << "symbol replaced" << endl;
+			}
+		}
+		if (isReplace == false)
+		{
+			cout << "there is no such symbol." << endl;
+		}
+	}
 };
 
 int main()
 {
-	MyString str1("123");
-	str1.print();
-	MyString str2("456");
-	str2.print();
+	MyString str1("123"); //copy
+	MyString str2 = "133"; //copy
 
-	/*cout << "_____________________________" << endl;
-	
-	str1 = "hello";
+	MyString str3 = str1 + str2; // + and copy
 
-	str1.print();
-	str2.print();*/
+	str1.replace('1', '2');//replaces '1' with '2'
+	str1.print();//print string
 
-	MyString str3 = str1 + str2;
-	str3.print();
+	str1[0] = 'Q';// overloaded operator [] returns char and replace str[0] with 'Q'
+	cout << str1[0] << endl;//demonstration of the overloaded operator []
 	return 0;
 }
